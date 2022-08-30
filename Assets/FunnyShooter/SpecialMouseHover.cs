71f3210e5d8c435b36ac6f32da9f013e8cef987a
wutSalty <mcminigame04@gmail.com>
2022-08-30 21:06:28 +1000

    Added animation to upgrade addition text
    + Added animation to upgrade addition text
    + Changed behaviour on how mouse handles with buttons
    + Mouse hover now selects the button instead of being highlighted in upgrade menu. Prevents two of the upgrades showing at once
    
     - Enlarge minimap with another button

--- Assets/FunnyShooter/SpecialMouseHover.cs
+++ Assets/FunnyShooter/SpecialMouseHover.cs
@@ -12,31 +12,44 @@ public class SpecialMouseHover : MonoBehaviour, IPointerEnterHandler, IPointerEx
     public string WhatShouldTheTextSay;
     [TextArea(2, 5)]
     public string DefaultText = "";
+    public Animator textAnim;
+
+    private Selectable selectable;
+
+    private void Start()
+    {
+        selectable = GetComponent<Selectable>();
+    }
 
     public void Update()
     {
         if (eventSystem.currentSelectedGameObject == gameObject)
         {
             AboutText.text = WhatShouldTheTextSay;
-        } 
-        else if (eventSystem.firstSelectedGameObject == null)
-        {
-            AboutText.text = DefaultText;
+            textAnim.Play("Text_Open");
         }
     }
 
     public void OnPointerEnter(PointerEventData eventData)
     {
+        selectable.Select();
+        EventSystem.current.firstSelectedGameObject = gameObject;
+
         AboutText.text = WhatShouldTheTextSay;
+        textAnim.Play("Text_Open");
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
-        AboutText.text = DefaultText;
+        EventSystem.current.SetSelectedGameObject(null);
+
+        //AboutText.text = DefaultText;
+        textAnim.Play("Text_Close");
     }
 
     public void OnDeselect(BaseEventData baseEvent)
     {
-        AboutText.text = DefaultText;
+        //AboutText.text = DefaultText;
+        textAnim.Play("Text_Close");
     }
 }
